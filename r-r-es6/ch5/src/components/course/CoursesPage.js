cf04d2bf15a323adf3b807133eaf5f845acda87d
andi <andi@andilabs.com>
2017-11-22 13:13:42 +0100

    mocks and thunk api consumption

--- r-r-es6/ch5/src/components/course/CoursesPage.js
+++ r-r-es6/ch5/src/components/course/CoursesPage.js
@@ -7,42 +7,17 @@ import PropTypes from 'prop-types'
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            course: { title: '' }
-        };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event){
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course})
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
         return <div key={index}>{course.title}</div>;
     }
+
     render() {
         return (
           <div>
               <h1>Courses</h1>
               {this.props.courses.map(this.courseRow)}
-              <h2>Add courses</h2>
-              <input
-                type="text"
-                onChange={this.onTitleChange}
-                value={this.state.course.title} />
-
-              <input
-                  type="submit"
-                  value="Save"
-                  onClick={this.onClickSave} />
           </div>
         );
     }
@@ -68,7 +43,3 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
-
-// alternative way of defining connect
-// const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
-// export default connectedStateAndProps(CoursesPage);
